143f33a0b2a65c3e59a1a214ba5607edb64f7d2a
Larissa Sampaio <larisampaio@hotmail.com>
2022-03-21 20:48:46 -0300

    Upgarde com GET

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("oi, gente");
+console.log("...SERVIDOR...");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/usuarios/:id', (req, res) => {
+    let id = req.params.id
+    res.send('Lista de usuário:' +id)
+  })
+
+app.listen(port, () => {
+  console.log(`Servidor escutando na porta ${port}`)
+})
